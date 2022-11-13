c1562e63c30d68b8e6a1eccbcc0362b976f4f163
Nhã Huỳnh <huynhthanhnha24111999@gmail.com>
2022-11-13 22:31:11 +0700

    update

---
+++ lib/tracking_input.dart
@@ -0,0 +1,73 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'input_helper.dart';
+
+typedef CaretMoved = void Function(Offset? globalCaretPosition);
+typedef TextChanged = void Function(String text);
+
+// Helper widget to track caret position.
+class TrackingTextInput extends StatefulWidget {
+  TrackingTextInput(
+      {Key? key,
+      required this.onCaretMoved,
+      required this.onTextChanged,
+      required this.hint,
+      required this.label,
+      this.isObscured = false})
+      : super(key: key);
+  final CaretMoved onCaretMoved;
+  final TextChanged onTextChanged;
+  final String hint;
+  final String label;
+  final bool isObscured;
+  @override
+  _TrackingTextInputState createState() => _TrackingTextInputState();
+}
+
+class _TrackingTextInputState extends State<TrackingTextInput> {
+  final GlobalKey _fieldKey = GlobalKey();
+  final TextEditingController _textController = TextEditingController();
+  late Timer _debounceTimer;
+  @override
+  initState() {
+    _textController.addListener(() {
+      // We debounce the listener as sometimes the caret position is updated after the listener
+      // this assures us we get an accurate caret position.
+      if (_debounceTimer.isActive ?? false) _debounceTimer.cancel();
+      _debounceTimer = Timer(const Duration(milliseconds: 100), () {
+        if (_fieldKey.currentContext != null) {
+          // Find the render editable in the field.
+          final RenderObject? fieldBox =
+              _fieldKey.currentContext?.findRenderObject();
+          Offset? caretPosition = getCaretPosition(fieldBox!);
+
+          if (widget.onCaretMoved != null) {
+            widget.onCaretMoved(caretPosition);
+          }
+        }
+      });
+      if (widget.onTextChanged != null) {
+        widget.onTextChanged(_textController.text);
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: TextFormField(
+          decoration: InputDecoration(
+            hintText: widget.hint,
+            labelText: widget.label,
+          ),
+          key: _fieldKey,
+          controller: _textController,
+          obscureText: widget.isObscured,
+          validator: (value) {}),
+    );
+  }
+}
